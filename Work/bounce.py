a35e623a8157bed1c9d6adea9402961ae2c0d220
korolm07 <59730491+korolm07@users.noreply.github.com>
2024-03-01 15:14:59 +0100

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce = 3/5
+height_start = 100
+height = height_start*bounce
+number = 1
+while number < 11:
+  print(number,round(height,4))
+  number = number + 1
+  height = height * bounce
